bc0d9dbab732f53699649c9dda217c61105afb18
davlatovv <zafargithub@gmail.com>
2023-01-18 17:20:27 +0500

    Ternary operstors

--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -22,7 +22,7 @@ public class SwitchDaysInMonthPrinter {
                 System.out.println(30);
                 break;
             default:
-                System.out.println("Incorrect number");
+                System.out.println("wrong number!");
         }
     }
 }
